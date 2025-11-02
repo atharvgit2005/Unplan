2e3bab7c1dddf6b7c6e6c75c9457eb42b69ca377
Khyati-Kapil <khyatekapil@gmail.com>
2025-11-02 13:06:46 +0530

    Made the trip controller and router

---
+++ backend/src/routes/tripRoutes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  createTrip,
+  getAllTrips,
+  getTripById,
+  joinTrip,
+  leaveTrip,
+} from "../controllers/tripController.js"; 
+
+const router = express.router();
+
+router.post('/',createTrip);
+router.get("/", getAllTrips); 
+router.get("/:id", getTripById);
+router.put("/:id/join", joinTrip);
+router.put("/:id/leave", leaveTrip); 
+
+export default router;
