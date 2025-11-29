1789ba69d94143ecaece73fa95accd1e7ca724bc
Khyati-Kapil <khyatekapil@gmail.com>
2025-11-30 01:27:17 +0530

    Login Fixes and connection to create trip dashboard

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -60,7 +60,7 @@ export default function Signup() {
           : [],
       };
 
-      const response = await fetch("http://localhost:8000/api/auth/signup", {
+      const response = await fetch("/api/auth/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
